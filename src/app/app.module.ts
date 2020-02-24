5192ca7ea5f9fc60de8772c0b992ce97960a1228
cmsusergit <cmsusergit@gmail.com>
2020-02-24 22:55:29 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { Test1Component } from './test1/test1.component';
 import { MaincontentComponent } from './maincontent/maincontent.component';
+import { BdetailComponent } from './bdetail/bdetail.component';
 @NgModule({
   imports:      [ BrowserModule, 
   NgbModule,
@@ -15,7 +16,8 @@ import { MaincontentComponent } from './maincontent/maincontent.component';
     AppComponent, 
     HelloComponent, 
     MaincontentComponent,
-    Test1Component ],
+    Test1Component,
+    BdetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/bdetail/bdetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bdetail',
+  templateUrl: './bdetail.component.html',
+  styleUrls: ['./bdetail.component.css']
+})
+export class BdetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
